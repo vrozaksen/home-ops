9b63791d15047c986f0a88b9ca71a82701d915ff
Vrozaksen <vrozaksen@protonmail.com>
2025-03-06 07:10:39 +0100

    shut down qbt

--- hack/nas-restart.sh
+++ hack/nas-restart.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-CLUSTER=${1:-main}
-kubectl get deployments --all-namespaces -l nfsMount=true -o custom-columns="NAMESPACE:.metadata.namespace,NAME:.metadata.name" --no-headers --context $CLUSTER  | awk '{print "kubectl rollout restart deployment/"$2" -n "$1} --context $CLUSTER ' | sh
+kubectl get deployments --all-namespaces -l nfsMount=true -o custom-columns="NAMESPACE:.metadata.namespace,NAME:.metadata.name" --no-headers  | awk '{print "kubectl rollout restart deployment/"$2" -n "$1} ' | sh
